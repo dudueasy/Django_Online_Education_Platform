72b43e8b149670a1a98b9ce50931974ba7456866
dudueasy <dudueasy@126.com>
2017-11-30 19:51:53 +0800

    对xadmin后台管理页面进行了全局设置

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,10 +3,20 @@ __author__ = 'apolo'
 _date_ = '2017/11/29 下午7:02'
 
 import xadmin
+from xadmin import views
 
 from .models import EmailVerifyRecord
 from .models import Banner
 
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+class GlobalSetting(object):
+    site_title = '小学僧在线后台管理系统'
+    site_footer = '小学僧在线网'
+    menu_style = 'accordion'
 
 class EmailVerifyRecordAdmin(object):
 
@@ -14,11 +24,15 @@ class EmailVerifyRecordAdmin(object):
     search_fields = ['code', 'email', 'send_type']
     list_filter = ['code','email','send_type','send_time']
 
+
 class BannerAdmin(object):
 
     list_display = ['title','image','url','index','add_time']
     search_fields = ['title','image','url','index']
     list_filter = ['title','image','url','index','add_time']
 
+
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSetting)
